dcfd7f6ea77fb5aea286f33fa62d32cd70f48731
Felipe Gutierrez Romero <felipegutiromero@gmail.com>
2019-07-18 20:49:13 -0500

    Cambios para poder mostrar el usuario en pantalla

---
+++ secciones/home.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+
+$usuario = $_SESSION['usuario'];
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="../navbar.css">
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/96b6886ff5.js"></script>
+    <title>Sitesc</title>
+</head>
+<body>
+    <div class="main">
+        <!--Navbar-->
+        <div class="navbar-main">
+            <div class="bar-btn" id="sub-menu-btn">
+                <i class="fas fa-bars" id="bar-icon"></i>
+            </div>
+        </div>
+        <!--Submenu-->
+        <div class="submenu" id="submenu">
+            <a href="home.html" class="submenu-item">Inicio</a>
+            <a href="asistencia.html" class="submenu-item">Pase de asistencia</a>
+            <a href="reporte.html" class="submenu-item">Generar Reportes</a>
+            <a href="admin.html" class="submenu-item">Administracion</a>
+            <a href="login.html" class="submenu-item">Inicio de sesión</a>
+        </div>
+
+        <!--Botones demas opciones-->
+        <div class="buttons">
+            <h1 class="titular">Bienvenido! <span><?php echo $usuario; ?></span></h1>
+            <a href="asistencia.html" class="btn-items"> <i class="fas fa-user-friends"></i> Pase de asistencia</a>
+            <a href="reporte.html" class="btn-items"> <i class="fas fa-book"></i> Generar Reportes</a>
+            <a href="admin.html" class="btn-items"><i class="fas fa-user-cog"></i> Administracion</a>
+            <a href="login.html" class="btn-items"><i class="fas fa-key"></i> Inicio de Sesión</a>
+        </div>
+    </div>
+
+    <script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
+    <script src="../submenu.js"></script>
+</body>
+</html>
