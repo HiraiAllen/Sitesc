8ebe122b63c112f03624f5b362179189524fde6c
Felipe Gutierrez Romero <felipegutiromero@gmail.com>
2019-08-13 15:00:19 -0500

    Agregada la funcionalidad del reporte, ya se envian a la BDD

--- secciones/reporte.php
+++ secciones/reporte.php
@@ -1,7 +1,50 @@
 <?php
 session_start();
+require "../conexion.php";
 
 $tipo_usuario = $_SESSION['tipo_usuario'];
+
+//Query para los maestros
+$query_maestros = 'SELECT * FROM maestros';
+$resultado_maestros = mysqli_query($connect, $query_maestros);
+
+
+//Query para tipo de reporte
+$query_tipo_reporte = 'SELECT * FROM tiporep';
+$resultado_tipo_reporte = mysqli_query($connect, $query_tipo_reporte);
+
+
+if(isset($_POST["btn_agregar"]))
+{
+    //Query estudiantes / Validación
+    $nombre = $_POST["nombre"];
+    $query_estudiantes = "SELECT nombre FROM asistencia WHERE '".$nombre."';";
+    $resultado_estudiantes = mysqli_query($connect, $query_estudiantes);
+    //Compruebo que el nombre del estudiante exista en la tabla para poder enviar los datos
+    //correctamente.
+    if($resultado_estudiantes != NULL)
+    {
+        $date = date('Y-m-d');
+        $grupo = $_POST["grupo"];
+        $tipo = $_POST["tipo"];
+        $profesor = $_POST["maestro"];
+        $dias_justificados = $_POST["dias-justificados"];
+        $domiciliaria = $_POST["visita-dom"];
+        $observaciones = $_POST["observaciones"];
+        $citatorio = $_POST["citatorio"];
+
+        $insertar_estudiante = 'INSERT INTO reportes(`fecha`, `grupo`, `maestro`, `nombre`, `reporte`, `com`, `cita`, `observ`, `dias`) 
+                                VALUES("'.$date.'", "'.$grupo.'","'.$profesor.';", "'.$nombre.'", "'.$tipo.'", "'.$domiciliaria.'",
+                                "'.$citatorio.'", "'.$observaciones.'", "'.$dias_justificados.'")';
+        $result_ins_estudiante = mysqli_query($connect,$insertar_estudiante);
+        if($result_ins_estudiante)
+        {
+            echo "<script>alert('reporte capturado');</script>";
+        }
+    }else{
+        echo "El estudiante no existe";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,42 +78,64 @@ $tipo_usuario = $_SESSION['tipo_usuario'];
         </div>
 
         <!--Formulario generador de reportes-->
-        <form action="" class="gen-reporte">
+        <form action="reporte.php" class="gen-reporte" method="POST">
             <label for="nombre" class="label-form">Nombre del estudiante</label>
-            <input type="text" id="nombre" class="inp-form">
+            <input type="text" id="nombre" class="inp-form" name="nombre">
             
             <label for="grupo" class="label-form">Grupo</label>
-            <select name="" id="grupo" class="select-rep">
+            <select name="grupo" id="grupo" class="select-rep">
+                <option value="1A">1A</option>
                 <option value="1B">1B</option>
+                <option value="1C">1C</option>
+                <option value="1D">1D</option>
+                <option value="2A">2A</option>
+                <option value="2B">2B</option>
+                <option value="2C">2C</option>
+                <option value="2D">2D</option>
+                <option value="3A">3A</option>
+                <option value="3B">3B</option>
+                <option value="3C">3C</option>
+                <option value="3D">3D</option>
             </select>
 
             <label for="tipo" class="label-form">Tipo</label>
-            <select name="" id="tipo" class="select-rep">
-                <option value="INASISTENCIA">INASISTENCIA</option>
+            <select name="tipo" id="tipo" class="select-rep">
+            <?php foreach($resultado_tipo_reporte as $key => $tipo_reporte): ?>
+                <option value="<?php echo utf8_encode($tipo_reporte["reporte"]);?>">
+                <?php echo utf8_encode($tipo_reporte["reporte"]);  ?></option>
+            <?php endforeach;?>
             </select>
 
             <label for="maestro" class="label-form">Maestro</label>
-            <select name="" id="maestro" class="select-rep">
-                <option value="JAIME HURTADO">JAIME HURTADO</option>
+            <select name="maestro" id="maestro" class="select-rep">
+            <?php foreach ($resultado_maestros as $key => $maestro): ?>
+                <option value="<?php echo utf8_encode($maestro["maestro"]);?>">
+                <?php echo utf8_encode($maestro["maestro"]);  ?></option>
+            <?php endforeach; ?>
             </select>
 
             <label for="dias-justificados" class="label-form">Dias justificados</label>
-            <input type="text" name="" id="dias-justificados" class="inp-form">
+            <input type="text" id="dias-justificados" class="inp-form" name="dias-justificados">
 
             <label for="visita-dom" class="label-form">Visita domiciliaria</label>
-            <input type="text" id="visita-dom" class="inp-form">
+            <input type="text" id="visita-dom" class="inp-form" name="visita-dom">
             
             <label for="observaciones" class="label-form">Observaciones</label>
-            <input type="text" id="observaciones" class="inp-form">
+            <input type="text" id="observaciones" class="inp-form" name="observaciones">
 
             <label for="citatorio" class="label-form">Citatorio</label>
-            <select name="" id="" class="select-rep">
-                <option value="texto">Texto</option>
-            </select>
+            
+            <input type="date" name="citatorio" class="select-rep">
 
-            <input type="submit" value="Agregar" class="btn-send">
+            <input type="submit" value="Agregar" name="btn_agregar" class="btn-send">
         </form>
     </div>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 
     <script
     src="https://code.jquery.com/jquery-3.4.1.min.js"
